7288f13f485dd957c9a8c5da36feefc49c35bfcf
Andrew Parkin <legsbot@gmail.com>
2011-04-26 21:24:56 +1000

    Initial commit of Eclipse RCP example

---
+++ au/org/legs/testrcp/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,18 @@
+package au.org.legs.testrcp;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "au.org.legs.TestRcp.perspective"; //$NON-NLS-1$
+
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+}
