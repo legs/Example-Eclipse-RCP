e1cfc738ea91f797104c0242069a1e08ed9bdc6f
Andrew Parkin <legsbot@gmail.com>
2011-04-29 21:54:50 +1000

    Incomplete prior checkin.

---
+++ au/org/legs/testrcp/customer/Person.java
@@ -0,0 +1,25 @@
+package au.org.legs.testrcp.customer;
+
+public class Person {
+	private String firstName;
+	private String lastName;
+	
+	public Person(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+}
